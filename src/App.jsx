862e629b87b93cb2fef6cef3cd2af28514974245
HPaulsrud <havard.paulsrud@gmail.com>
2024-03-20 11:58:39 +0100

    Opprettet Layoutbranch. Laget og kompilert sass-filer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './css/main.css';
 //Kilde til Routing: https://www.w3schools.com/react/react_router.asp
 import {BrowserRouter, BrowserRouter as Routes, Route} from 'react-router-dom'
 import Navbar from './Navbar'
